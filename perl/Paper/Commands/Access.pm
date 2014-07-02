8c3d5532b5d3e35a3a356419172fd5c6b4cbb784
Dan Book <grinnz@grinnz.com>
2014-07-02 14:04:53 -0400

    paper object singleton; split access constants to own module

---
+++ perl/Paper/Commands/Access.pm
@@ -0,0 +1,29 @@
+package Paper::Commands::Access;
+
+use strict;
+use warnings;
+use Exporter qw/import/;
+
+use constant ACCESS_LEVELS => {
+	ACCESS_NONE => 0,
+	ACCESS_VOICE => 1,
+	ACCESS_HALFOP => 2,
+	ACCESS_OP => 3,
+	ACCESS_CHANADMIN => 4,
+	ACCESS_BOTADMIN => 5,
+	ACCESS_MASTER => 6
+};
+
+use constant ACCESS_LEVELS();
+
+our @EXPORT = (keys %{ACCESS_LEVELS()}, 'valid_access_level');
+
+my %level_nums;
+$level_nums{$_} = 1 foreach values %{ACCESS_LEVELS()};
+
+sub valid_access_level {
+	my $level = shift // return undef;
+	return exists $level_nums{$level};
+}
+
+1;
